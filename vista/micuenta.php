83db9626e2aabf934c2420eeb432266fd8d5a86d
FaiberHernandez <93232600+FaiberHernandez@users.noreply.github.com>
2021-12-10 18:03:43 -0500

    boxShadowAndtitleSwal MiCuenta

--- vista/micuenta.php
+++ vista/micuenta.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['ID_USUARIO'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSS/miCuenta.css">
+    <link rel="stylesheet" type="text/css" href="CSS/estilos.css">
     <link rel="stylesheet" type="text/css" href="CSS/userLog.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
